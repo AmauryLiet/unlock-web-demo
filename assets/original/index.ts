03ba97f3d3095634d8527ac5a49522725384efc9
amauryliet <amaury.liet@hotmail.fr>
2020-04-10 00:06:16 +0200

    feat: Convert all pdf in script

---
+++ assets/original/index.ts
@@ -0,0 +1,28 @@
+export const CARDS_PER_PAGE = 6;
+
+export const pdfAssets: Array<{ filename: string; emptyCardsCountOnLastPage: number }> = [
+  {
+    filename: "0 - tutorial.pdf",
+    emptyCardsCountOnLastPage: 2,
+  },
+  {
+    filename: "1 - 5ème avenue.pdf",
+    emptyCardsCountOnLastPage: 3,
+  },
+  {
+    filename: "2 - Le bonjon de Doo-Arann.pdf",
+    emptyCardsCountOnLastPage: 0,
+  },
+  {
+    filename: "3 - La chambre d'hôtel.pdf",
+    emptyCardsCountOnLastPage: 4,
+  },
+  {
+    filename: "4 - A la poursuite de Cabrakan.pdf",
+    emptyCardsCountOnLastPage: 0,
+  },
+  {
+    filename: "5 - L’autel du dieu RA.pdf",
+    emptyCardsCountOnLastPage: 0,
+  },
+];
--- scripts/pdfToPngConverter.ts
+++ scripts/pdfToPngConverter.ts
@@ -1,17 +1,23 @@
 import PDF2Pic from "pdf2pic";
+import { pdfAssets } from "../assets/original";
 
 const LARGER_DIMENSION_SIZE = 600;
 
-const pdf2pic = new PDF2Pic({
-  density: 100, // output pixels per inch
-  savename: "converted_pdf", // output file name
-  savedir: "./assets/pages", // output file location
-  format: "png", // output file format
-  size: `${LARGER_DIMENSION_SIZE}x${LARGER_DIMENSION_SIZE}`, // output size in pixels
-});
-
-pdf2pic
-  .convertBulk(`./assets/original/0 - tutorial.pdf`, -1)
-  .then((resolve) => {
-    console.log("pdf converted successfully!");
+pdfAssets.map(({ filename, emptyCardsCountOnLastPage }) => {
+  const pdf2pic = new PDF2Pic({
+    density: 100, // output pixels per inch
+    savename: filename, // output file name
+    savedir: "./assets/pages", // output file location
+    format: "png", // output file format
+    size: `${LARGER_DIMENSION_SIZE}x${LARGER_DIMENSION_SIZE}`, // output size in pixels
   });
+
+  pdf2pic
+    .convertBulk(`./assets/original/${filename}`, -1)
+    .then((resolve) => {
+      console.log("pdf converted successfully!");
+    })
+    .catch((error) => {
+      console.error(`cannot convert pdf>png for file "${filename}":`, error);
+    });
+});
