9f28ef2a69331386ca8618249a3d0cb6ced7d812
amauryliet <amaury.liet@hotmail.fr>
2020-04-11 18:23:39 +0200

    refactor: Rename var with a too vague naming

--- scripts/pdfToPngConverter.ts
+++ scripts/pdfToPngConverter.ts
@@ -21,50 +21,52 @@ interface PageAssetsMetadata {
   pagesPaths: string[];
 }
 
-pdfAssetsMetadata.forEach(async ({ filename, emptyCardsCountOnLastPage }) => {
-  const pdf2pic = new PDF2Pic({
-    density: 100, // output pixels per inch
-    savename: filename, // output file name
-    savedir: pagesAssetsDir, // output file location
-    format: "png", // output file format
-    size: `${LARGER_DIMENSION_SIZE}x${LARGER_DIMENSION_SIZE}`, // output size in pixels
-  });
+pdfAssetsMetadata.forEach(
+  async ({ filename: pdfFilename, emptyCardsCountOnLastPage }) => {
+    const pdf2pic = new PDF2Pic({
+      density: 100, // output pixels per inch
+      savename: pdfFilename, // output file name
+      savedir: pagesAssetsDir, // output file location
+      format: "png", // output file format
+      size: `${LARGER_DIMENSION_SIZE}x${LARGER_DIMENSION_SIZE}`, // output size in pixels
+    });
 
-  let pageAssetsMetadata: PageAssetsMetadata;
-  try {
-    const conversionResults = await pdf2pic.convertBulk(
-      path.join(pdfAssetsDir, `${filename}.pdf`),
-      -1
-    );
-    console.info(`✅ pdf "${filename}" successfully converted in png`);
+    let pageAssetsMetadata: PageAssetsMetadata;
+    try {
+      const conversionResults = await pdf2pic.convertBulk(
+        path.join(pdfAssetsDir, `${pdfFilename}.pdf`),
+        -1
+      );
+      console.info(`✅ pdf "${pdfFilename}" successfully converted in png`);
 
-    pageAssetsMetadata = {
-      name: filename,
-      emptyCardsCountOnLastPage,
-      pagesPaths: conversionResults.map((result) => result.path),
-    };
-  } catch (error) {
-    console.error(`cannot convert pdf>png for file "${filename}":`, error);
-    return;
+      pageAssetsMetadata = {
+        name: pdfFilename,
+        emptyCardsCountOnLastPage,
+        pagesPaths: conversionResults.map((result) => result.path),
+      };
+    } catch (error) {
+      console.error(`cannot convert pdf>png for file "${pdfFilename}":`, error);
+      return;
+    }
+
+    const pdfCardsAssetsDir = path.join(cardsAssetsDir, pdfFilename);
+
+    if (!fs.existsSync(pdfCardsAssetsDir)) fs.mkdirSync(pdfCardsAssetsDir);
+
+    pageAssetsMetadata.pagesPaths.map(async (pagePath, index) => {
+      const page = sharp(pagePath);
+      const { width, height } = await page.metadata();
+
+      const top = Math.round(PDF_VERTICAL_MARGIN * height),
+        left = Math.round(PDF_HORIZONTAL_MARGIN * width);
+      await page
+        .extract({
+          left,
+          top,
+          width: width - 2 * left,
+          height: height - 2 * top,
+        })
+        .toFile(path.join(pdfCardsAssetsDir, "output.png"));
+    });
   }
-
-  const pdfCardsAssetsDir = path.join(cardsAssetsDir, filename);
-
-  if (!fs.existsSync(pdfCardsAssetsDir)) fs.mkdirSync(pdfCardsAssetsDir);
-
-  pageAssetsMetadata.pagesPaths.map(async (pagePath, index) => {
-    const page = sharp(pagePath);
-    const { width, height } = await page.metadata();
-
-    const top = Math.round(PDF_VERTICAL_MARGIN * height),
-      left = Math.round(PDF_HORIZONTAL_MARGIN * width);
-    await page
-      .extract({
-        left,
-        top,
-        width: width - 2 * left,
-        height: height - 2 * top,
-      })
-      .toFile(path.join(pdfCardsAssetsDir, "output.png"));
-  });
-});
+);
