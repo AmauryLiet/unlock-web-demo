dae4c42f46949ab15d0408e749ffdc6dc90f196f
amauryliet <amaury.liet@hotmail.fr>
2020-04-11 16:55:58 +0200

    feat: Add/fix info about assets

--- assets/original/index.ts
+++ assets/original/index.ts
@@ -1,28 +1,47 @@
 export const CARDS_PER_PAGE = 6;
+export const PDF_VERTICAL_MARGIN = 8.7 / 100;
+export const PDF_HORIZONTAL_MARGIN = 6 / 100;
 
-export const pdfAssetsMetadata: Array<{ filename: string; emptyCardsCountOnLastPage: number }> = [
+export const pdfAssetsMetadata: Array<{
+  filename: string;
+  emptyCardsCountOnLastPage: number;
+  startWithSecretFaces: boolean;
+  introductionCards: number;
+}> = [
   {
     filename: "0 - tutorial",
     emptyCardsCountOnLastPage: 2,
+    startWithSecretFaces: true,
+    introductionCards: 1,
   },
   {
     filename: "1 - 5ème avenue",
     emptyCardsCountOnLastPage: 3,
+    startWithSecretFaces: true,
+    introductionCards: 2,
   },
   {
-    filename: "2 - Le bonjon de Doo-Arann",
+    filename: "2 - Le donjon de Doo-Arann",
     emptyCardsCountOnLastPage: 0,
+    startWithSecretFaces: false,
+    introductionCards: 3,
   },
   {
     filename: "3 - La chambre d'hôtel",
     emptyCardsCountOnLastPage: 4,
+    startWithSecretFaces: true,
+    introductionCards: 1,
   },
   {
     filename: "4 - A la poursuite de Cabrakan",
     emptyCardsCountOnLastPage: 0,
+    startWithSecretFaces: false,
+    introductionCards: 3,
   },
   {
     filename: "5 - L’autel du dieu RA",
     emptyCardsCountOnLastPage: 0,
+    startWithSecretFaces: true,
+    introductionCards: 1,
   },
 ];
