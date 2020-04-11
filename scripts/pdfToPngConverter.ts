56772332bdb6c4b4832f9ec3a249e54fd92f701b
amauryliet <amaury.liet@hotmail.fr>
2020-04-11 13:58:27 +0200

    refactor: Centralize path + make it independant from runing pwd

--- scripts/pdfToPngConverter.ts
+++ scripts/pdfToPngConverter.ts
@@ -2,20 +2,24 @@ import path from "path";
 import PDF2Pic from "pdf2pic";
 import { pdfAssetsMetadata } from "../assets/original";
 
+const assetsDir = path.join(__dirname, "../assets");
+const pdfAssetsDir = path.join(assetsDir, "./original");
+const pagesAssetsDir = path.join(assetsDir, "./pages");
+
 const LARGER_DIMENSION_SIZE = 600;
 
 pdfAssetsMetadata.map(async ({ filename, emptyCardsCountOnLastPage }) => {
   const pdf2pic = new PDF2Pic({
     density: 100, // output pixels per inch
     savename: filename, // output file name
-    savedir: "./assets/pages", // output file location
+    savedir: pagesAssetsDir, // output file location
     format: "png", // output file format
     size: `${LARGER_DIMENSION_SIZE}x${LARGER_DIMENSION_SIZE}`, // output size in pixels
   });
 
   try {
     const conversionResults = await pdf2pic.convertBulk(
-      `./assets/original/${filename}.pdf`,
+      path.join(pdfAssetsDir, `${filename}.pdf`),
       -1
     );
     console.info("pdf successfully converted in png!");
