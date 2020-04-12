f630d9e86e4638f6459defcf90f072563bdc7bdc
amauryliet <amaury.liet@hotmail.fr>
2020-04-12 23:07:02 +0200

    feat: Display scenario details on its page

--- src/pages/scenario/[scenarioName].tsx
+++ src/pages/scenario/[scenarioName].tsx
@@ -1,9 +1,41 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { ConvertedAssetsMetadata } from "../../../scripts/pdfToPngConverter";
+
+const allConvertedAssetsMetadata: ConvertedAssetsMetadata[] = require("../../../public/metadata.json");
 
 export default () => {
   const router = useRouter();
   const { scenarioName } = router.query;
 
-  return <span>{`Welcome to the scenario "${scenarioName}"`}</span>;
+  const scenarioAssetsMetadata = allConvertedAssetsMetadata.find(
+    (convertedAssetsMetadata) =>
+      convertedAssetsMetadata.scenarioPublicName === scenarioName
+  );
+
+  if (!scenarioAssetsMetadata) {
+    return (
+      <div>{`Error: could not find assets with name "${scenarioName}". Available ones: ${allConvertedAssetsMetadata.map(
+        (data) => data.scenarioPublicName
+      )}`}</div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>{`Welcome to the scenario "${scenarioName}"`}</h2>
+      <h3>Initial pages paths</h3>
+      <ul>
+        {scenarioAssetsMetadata.introCards.map((cardMetadata) => (
+          <li>{cardMetadata.visibleSidePath}</li>
+        ))}
+      </ul>
+      <h3>Additional pages paths</h3>
+      <ul>
+        {scenarioAssetsMetadata.numberedCards.map((cardMetadata) => (
+          <li>{cardMetadata.visibleSidePath}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
