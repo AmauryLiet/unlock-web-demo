25f28c7db369c6ca5b231ca03d4c71ded3078d5f
amauryliet <amaury.liet@hotmail.fr>
2020-04-12 19:26:57 +0200

    feat: Add default nice font

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 export default () => {
   return (
     <div>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <p>Hello Next.tsx</p>
     </div>
   );
-}
+};
