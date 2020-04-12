cecb01e9ba0c2ff278a0121b2cbff1d404510558
amauryliet <amaury.liet@hotmail.fr>
2020-04-12 09:38:20 +0200

    refactor: Clarify that var value is about a number

--- assets/original/index.ts
+++ assets/original/index.ts
@@ -6,42 +6,42 @@ export const pdfAssetsMetadata: Array<{
   filename: string;
   emptyCardsCountOnLastPage: number;
   startWithSecretFaces: boolean;
-  introductionCards: number;
+  introductionCardsCount: number;
 }> = [
   {
     filename: "0 - tutorial",
     emptyCardsCountOnLastPage: 2,
     startWithSecretFaces: true,
-    introductionCards: 1,
+    introductionCardsCount: 1,
   },
   {
     filename: "1 - 5ème avenue",
     emptyCardsCountOnLastPage: 3,
     startWithSecretFaces: true,
-    introductionCards: 2,
+    introductionCardsCount: 2,
   },
   {
     filename: "2 - Le donjon de Doo-Arann",
     emptyCardsCountOnLastPage: 0,
     startWithSecretFaces: false,
-    introductionCards: 3,
+    introductionCardsCount: 3,
   },
   {
     filename: "3 - La chambre d'hôtel",
     emptyCardsCountOnLastPage: 4,
     startWithSecretFaces: true,
-    introductionCards: 1,
+    introductionCardsCount: 1,
   },
   {
     filename: "4 - A la poursuite de Cabrakan",
     emptyCardsCountOnLastPage: 0,
     startWithSecretFaces: false,
-    introductionCards: 3,
+    introductionCardsCount: 3,
   },
   {
     filename: "5 - L’autel du dieu RA",
     emptyCardsCountOnLastPage: 0,
     startWithSecretFaces: true,
-    introductionCards: 1,
+    introductionCardsCount: 1,
   },
 ];
