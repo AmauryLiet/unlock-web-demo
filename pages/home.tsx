f3ceafc839e6ab416553b8d30f0fae8912333461
amauryliet <amaury.liet@hotmail.fr>
2020-04-12 19:31:44 +0200

    feat: Use clean favicon & title

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,9 +4,15 @@ export default () => {
   return (
     <div>
       <Head>
+        <title>Unlock free demo</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
           rel="stylesheet"
+        />
+        <link
+          rel="shortcut icon"
+          href="https://static.wixstatic.com/media/59baa2_0ad2227e86b749e7b61facf1794f6af0%7Emv2_d_5016_5051_s_4_2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/59baa2_0ad2227e86b749e7b61facf1794f6af0%7Emv2_d_5016_5051_s_4_2.png"
+          type="image/png"
         />
       </Head>
       <p>Hello Next.tsx</p>
